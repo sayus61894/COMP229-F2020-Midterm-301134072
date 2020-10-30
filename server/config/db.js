ffbfb7b26bd4efac2f0516e9628d7d7097b55ed9
Parth Shreyash Patel <parth.shreyash.patel@gmail.com>
2020-10-30 11:57:25 -0400

    Integrated with mongoDB Atlas instead of local database

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-  //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  /* local MongoDB deployment -> */ //"URI": "mongodb://localhost/books229"
+  "URI" :"mongodb+srv://sayus:gkV6a8LtE8AoiVGe@mongodbserver.7cksp.mongodb.net/books229?retryWrites=true&w=majority"
 };
